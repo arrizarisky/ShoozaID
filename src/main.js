567b2281a9298400d9708db1935ce63b71cbdc20
mhdarriza1404 <mhdarriza1404@gmail.com>
2024-06-25 12:37:11 +0000

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import './style.css'
+import App from './App.vue'
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faShoppingCart)
+
+createApp(App).mount('#app');
